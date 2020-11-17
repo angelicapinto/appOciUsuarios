355ca0a8ea463ccf6729093debee5194c5c5a676
angelicapinto <angelicapinto@DESKTOP-NPN4SJS>
2020-11-17 08:56:37 -0500

    Primer commit

---
+++ appOciUsuarios/src/main/java/co/edu/unicauca/asae/core/AppOciUsuariosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOciUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOciUsuariosApplication.class, args);
+	}
+
+}
